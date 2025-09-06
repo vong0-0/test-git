b39ffeb0affcb8998b8d2d328ed11f9b16182344
vong <vongsouvan09@gmail.com>
2025-09-06 19:06:00 +0700

    Create feature say hi for say hi button

---
+++ script.js
@@ -0,0 +1,6 @@
+const sayHiBtn = document.querySelector(".say-hi-button");
+const greeting = document.querySelector(".greeting");
+
+sayHiBtn.addEventListener("click", () => {
+  greeting.textContent = "Hello world";
+});
